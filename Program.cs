fc70f40bb61129a3ceb80057cd1c5c656ea39069
gerdasandvall <122353900+gerdasandvall@users.noreply.github.com>
2023-10-09 14:49:19 +0200

    Added Comments

--- Program.cs
+++ Program.cs
@@ -1,63 +1,46 @@
 ﻿
 namespace gerdasCeasarChiffer
 {
-
     class Program
     {
-
-
+        //Mainmetod
         static void Main(string[] args)
         {
 
             bool isRunning = true;
-            //char c = (char)144;
-            //Console.WriteLine(c);
-            //tribute till hanna
 
             while (isRunning)
             {
-
-                int nKey = Random.Shared.Next(0, 195);
-                string userInput = Console.ReadLine();
+                int nKey = Random.Shared.Next(0, 195); //slumpar fram nyckeln
+                string userInput = Console.ReadLine(); //läser in användardata
                 char[] charArray = userInput.ToCharArray();
+                int[] decryptedNumbers = new int[charArray.Length]; //dekrypterade nummer för respektive tecken
+                int[] encryptedNumbers = new int[charArray.Length]; //krypterade nummer för respektive tecken
 
-
-                int[] decryptedNumbers = new int[charArray.Length];
-                int[] encryptedNumbers = new int[charArray.Length];
-
-                char[] encryptedChars = new char[charArray.Length];
-                char[] decryptedChars = new char[charArray.Length];
+                char[] decryptedChars = new char[charArray.Length]; //dekrypterade tecken
+                char[] encryptedChars = new char[charArray.Length]; //krypterade tecken
 
                 for (int i = 0; i < charArray.Length; i++)
                 {
-
-                    int j = (int)charArray[i] + nKey;
-                    encryptedNumbers[i] = j;
-                    encryptedChars[i] = (char)j;
-                    Console.Write(encryptedChars[i]);
-
+                    int j = (int)charArray[i] + nKey; //räknar fram ny position
+                    encryptedNumbers[i] = j; //sätter numret för position i array
+                    encryptedChars[i] = (char)j; //sätter tecknet för denna position
+                    Console.Write(encryptedChars[i]); //skriver ut tecken
                 }
 
+                Console.WriteLine(" "); 
+                Console.WriteLine("nyckel" + nKey); //skriver ut nyckeln
                 Console.WriteLine(" ");
-                Console.WriteLine("nyckel" + nKey);
 
-                for (int i = 0; i < charArray.Length; i++)
+                for (int i = 0; i < charArray.Length; i++) //skriver ut originaldata, dekrypterar mha nyckeln
                 {
-
-                    int j = (int)encryptedNumbers[i] - nKey;
-                    decryptedNumbers[i] = j;
-                    decryptedChars[i] = (char)j;
-                    Console.Write(decryptedChars[i]);
+                    int j = (int)encryptedNumbers[i] - nKey; //räknar fram originaltecknets position
+                    decryptedNumbers[i] = j;  //sätter numret för position i array
+                    decryptedChars[i] = (char)j; //sätter tecknet för denna position
+                    Console.Write(decryptedChars[i]); //skriver ut tecknet
                 }
-
-
-
-
             }
 
-
-
         }
-
     }
 }
