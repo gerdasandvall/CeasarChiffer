2c8d14e9d74afdf123a2b84b710e32c091305bd7
gerdasandvall <122353900+gerdasandvall@users.noreply.github.com>
2023-10-09 14:35:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+namespace gerdasCrackSuperUltraAwesome
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            bool isRunning = true;
+            //char c = (char)144;
+            //Console.WriteLine(c);
+            //tribute till hanna
+
+            while (isRunning)
+            {
+
+                int nKey = Random.Shared.Next(0, 195);
+                string userInput = Console.ReadLine();
+                char[] charArray = userInput.ToCharArray();
+
+
+                int[] decryptedNumbers = new int[charArray.Length];
+                int[] encryptedNumbers = new int[charArray.Length];
+
+                char[] encryptedChars = new char[charArray.Length];
+                char[] decryptedChars = new char[charArray.Length];
+
+                for (int i = 0; i < charArray.Length; i++)
+                {
+
+                    int j = (int)charArray[i] + nKey;
+                    encryptedNumbers[i] = j;
+                    encryptedChars[i] = (char)j;
+                    Console.Write(encryptedChars[i]);
+
+                }
+
+                Console.WriteLine(" ");
+                Console.WriteLine("nyckel" + nKey);
+
+                for (int i = 0; i < charArray.Length; i++)
+                {
+
+                    int j = (int)encryptedNumbers[i] - nKey;
+                    decryptedNumbers[i] = j;
+                    decryptedChars[i] = (char)j;
+                    Console.Write(decryptedChars[i]);
+                }
+
+
+
+
+            }
+
+
+
+        }
+
+    }
+}
