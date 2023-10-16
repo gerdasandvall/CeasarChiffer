8ae1066ddadb00ef8cafb57f766a6a08c0670ad6
gerdasandvall <122353900+gerdasandvall@users.noreply.github.com>
2023-10-16 15:28:29 +0200

    update <3

--- Program.cs
+++ Program.cs
@@ -6,20 +6,32 @@ namespace gerdasCeasarChiffer
         //Mainmetod
         static void Main(string[] args)
         {
-
+                Console.ForegroundColor = ConsoleColor.Blue;
             bool isRunning = true;
 
             while (isRunning)
             {
+                Console.WriteLine (@"
+   ____ U _____ u    _      ____       _       ____           ____   _   _               _____   _____ U _____ u   ____     
+U /""___|\| ___""|/U  /""\  u / __""| uU  /""\  uU |  _""\ u     U /""___| |'| |'|     ___     |"" ___| |"" ___|\| ___""|/U |  _""\ u  
+\| | u   |  _|""   \/ _ \/ <\___ \/  \/ _ \/  \| |_) |/     \| | u  /| |_| |\   |_""_|   U| |_  uU| |_  u |  _|""   \| |_) |/  
+ | |/__  | |___   / ___ \  u___) |  / ___ \   |  _ <        | |/__ U|  _  |u    | |    \|  _|/ \|  _|/  | |___    |  _ <    
+  \____| |_____| /_/   \_\ |____/>>/_/   \_\  |_| \_\        \____| |_| |_|   U/| |\u   |_|     |_|     |_____|   |_| \_\   
+ _// \\  <<   >>  \\    >>  )(  (__)\\    >>  //   \\_      _// \\  //   \\.-,_|___|_,-.)(\\,-  )(\\,-  <<   >>   //   \\_  
+(__)(__)(__) (__)(__)  (__)(__)    (__)  (__)(__)  (__)    (__)(__)(_"") (""_)\_)-' '-(_/(__)(_/ (__)(_/ (__) (__) (__)  (__) 
+");
+                Console.WriteLine("LOADING...");
+                Console.ReadKey();
                 int nKey = Random.Shared.Next(0, 195); //slumpar fram nyckeln
+                Console.WriteLine ("Skriv in datan du vill kryptera:");
                 string userInput = Console.ReadLine(); //läser in användardata
                 char[] charArray = userInput.ToCharArray();
                 int[] decryptedNumbers = new int[charArray.Length]; //dekrypterade nummer för respektive tecken
                 int[] encryptedNumbers = new int[charArray.Length]; //krypterade nummer för respektive tecken
-
                 char[] decryptedChars = new char[charArray.Length]; //dekrypterade tecken
                 char[] encryptedChars = new char[charArray.Length]; //krypterade tecken
 
+                Console.WriteLine ("Krypterad data:");
                 for (int i = 0; i < charArray.Length; i++)
                 {
                     int j = (int)charArray[i] + nKey; //räknar fram ny position
@@ -27,10 +39,8 @@ namespace gerdasCeasarChiffer
                     encryptedChars[i] = (char)j; //sätter tecknet för denna position
                     Console.Write(encryptedChars[i]); //skriver ut tecken
                 }
-
-                Console.WriteLine(" "); 
-                Console.WriteLine("nyckel" + nKey); //skriver ut nyckeln
-                Console.WriteLine(" ");
+                Console.WriteLine ("");
+            Console.WriteLine ("Den ursprungliga datan:");
 
                 for (int i = 0; i < charArray.Length; i++) //skriver ut originaldata, dekrypterar mha nyckeln
                 {
@@ -39,6 +49,8 @@ namespace gerdasCeasarChiffer
                     decryptedChars[i] = (char)j; //sätter tecknet för denna position
                     Console.Write(decryptedChars[i]); //skriver ut tecknet
                 }
+                     Console.ReadKey();
+                Console.Clear();
             }
 
         }
